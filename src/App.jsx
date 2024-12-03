109fd666af510643719d4faa7e3c22ff07e9aec8
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-03 15:42:29 +0100

    add list item

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 
 function App() {
+  const articles = [];
+
   return (
     <>
-      <div className="container text-center ">
-        <h1 className="m-5">ARTICOLI</h1>
-        <hr />
+      <div className="container text-start ">
+        <h1 className="mt-5 mb-4">ARTICOLI</h1>
+
         <form action="">
           <input
-            className="p-3 w-50"
+            className="p-2 w-50"
             type="text"
             placeholder="Inserisci titolo articolo..."
+            value={""}
           />
           <button className="btn btn-primary ms-3">Invia</button>
         </form>
+        <hr />
+        <ul className="text-start list-group">
+          <li className="list-group-item d-flex justify-content-between">
+            prova
+            <button className="btn btn-outline-danger btn-sm ms-3 ">
+              Elimina
+            </button>
+          </li>
+        </ul>
       </div>
     </>
   );
