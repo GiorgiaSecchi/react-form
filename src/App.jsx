d31443038c8ded2cb0cebeae321024c69769f361
GiorgiaSecchi <secchigrg@gmail.com>
2024-12-03 15:04:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center m-5">INSERISCI ARTICOLI</h1>
+        <hr />
+      </div>
+    </>
+  );
+}
+
+export default App;
